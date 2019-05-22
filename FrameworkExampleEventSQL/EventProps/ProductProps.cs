3c9a5a6cbd3590c7414bc537203ccee0d0f89767
Robert Moore <robert.ben.moore@gmail.com>
2019-05-22 15:07:31 -0700

    finished with productprops and tests

---
+++ FrameworkExampleEventSQL/EventProps/ProductProps.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using ToolsCSharp;
+using DBDataReader = System.Data.SqlClient.SqlDataReader;
+
+namespace EventPropsClasses
+{
+    [Serializable()]
+    public class ProductProps : IBaseProps
+    {
+
+        #region instance variables - all the fields in the current database
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ID = Int32.MinValue;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string prodCode = "";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string description = "";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public decimal unitPrice = Decimal.MinValue;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int onHandQuantity = Int32.MinValue;
+
+        /// <summary>
+        /// ConcurrencyID. See main docs, don't manipulate directly
+        /// </summary>
+        public int ConcurrencyID = 0;
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// Constructor. This object should only be instantiated by Customer, not used directly.
+        /// </summary>
+        public ProductProps()
+        {
+        }
+
+        #endregion
+
+        #region BaseProps Members
+        /// <summary>
+        /// Serializes this props object to XML, and writes the key-value pairs to a string.
+        /// It would be possible to make this return a json instead of an xml
+        /// </summary>
+        /// <returns>String containing key-value pairs</returns>	
+        public string GetState()
+        {
+            XmlSerializer serializer = new XmlSerializer(this.GetType());
+            StringWriter writer = new StringWriter();
+            serializer.Serialize(writer, this);
+            return writer.GetStringBuilder().ToString();
+        }
+
+        // I don't always want to generate xml in the db class so the 
+        // props class can read in from xml
+        public void SetState(DBDataReader dr)
+        {
+            this.ID = (Int32)dr["ProductID"];
+            this.prodCode = (string)dr["ProductCode"];
+            this.description = (string)dr["Description"];
+            this.unitPrice = (decimal)dr["UnitPrice"];
+            this.onHandQuantity = (int)dr["OnHandQuantity"];
+            this.ConcurrencyID = (Int32)dr["ConcurrencyID"];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void SetState(string xml)
+        {
+            XmlSerializer serializer = new XmlSerializer(this.GetType());
+            StringReader reader = new StringReader(xml);
+            ProductProps p = (ProductProps)serializer.Deserialize(reader);
+            this.ID = p.ID;
+            this.prodCode = p.prodCode;
+            this.description = p.description;
+            this.unitPrice = p.unitPrice;
+            this.onHandQuantity = p.onHandQuantity;
+            this.ConcurrencyID = p.ConcurrencyID;
+        }
+        #endregion
+
+        #region ICloneable Members
+        /// <summary>
+        /// Clones this object. This is needed because IBaseProps
+        /// has ICloneable, so it must be implemented in child class
+        /// </summary>
+        /// <returns>A clone of this object.</returns>
+        public Object Clone()
+        {
+            ProductProps p = new ProductProps();
+            p.ID = this.ID;
+            p.prodCode = this.prodCode;
+            p.description = this.description;
+            p.unitPrice = this.unitPrice;
+            p.onHandQuantity = this.onHandQuantity;
+            p.ConcurrencyID = this.ConcurrencyID;
+            return p;
+        }
+        #endregion
+
+    }
+}
---
+++ FrameworkExampleEventSQL/EventTestClasses/ProductPropsTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventPropsClasses;
+using NUnit.Framework;
+
+namespace EventTestClasses
+{
+    [TestFixture]
+    public class ProductPropsTests
+    {
+        // declare field variables
+        ProductProps props1;
+        ProductProps props2;
+
+        [SetUp]
+        public void TestSetUp()
+        {
+            props1 = new ProductProps();
+            props1.ID = 1;
+            props1.prodCode = "fgf5";
+            props1.description = "South Sea Islands";
+            props1.unitPrice = 30.00M;
+            props1.onHandQuantity = 10;
+            props1.ConcurrencyID = 4;
+
+            props2 = new ProductProps();
+        }
+
+        [Test]
+        public void TestClone()
+        {
+            // must cast to CustomerProps because Clone() returns an object
+            ProductProps props2 = (ProductProps)props1.Clone();
+
+            Assert.NotNull(props2);
+
+            // change props1 description to Oceanic Languages"
+            props1.description = "Oceanic Languages";
+
+            Assert.AreNotEqual(props1.description, props2.description);
+            Assert.AreNotSame(props1, props2);
+        }
+        [Test]
+        public void TestGetState()
+        {
+            string xml = props1.GetState();
+            Console.WriteLine(xml);
+        }
+
+        [Test]
+        public void TestSetState()
+        {
+            string xml = props1.GetState();
+            props2.SetState(xml);
+
+            Assert.AreEqual(props1.description, props2.description);
+            Assert.AreEqual(props1.unitPrice, props2.unitPrice);
+        }
+    }
+}
